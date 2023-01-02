36690f3c688d657ad486f9041765a11d07775ac0
Hossein Eshraghi <eshraghi4office@gmail.com>
2023-01-02 17:56:31 +0330

    fix answer two for check even and odd num

--- hacker-rank-answer/02.find-odd-and-even-num.py
+++ hacker-rank-answer/02.find-odd-and-even-num.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
 if n % 2 == 0:
         if 2 < n < 5:
             print("Not Weird")
-        elif 6 < n < 20:
+        elif 6 <= n <= 20:
             print("Weird")
         elif n > 20:
             print("Not Weird")
 # condition for add numbers
-else:
-    print("Werid")
+elif n % 2 != 0:
+    print("Weird")
