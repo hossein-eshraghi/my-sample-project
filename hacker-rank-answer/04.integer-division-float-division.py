b2445915fa46902552ee2449631e1d37a5083405
Hossein Eshraghi <eshraghi4office@gmail.com>
2023-01-08 18:49:57 +0330

    add answer 04 for hackerrank

---
+++ hacker-rank-answer/04.integer-division-float-division.py
@@ -0,0 +1,14 @@
+# The provided code stub reads two integers, a and b, from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division,
+# The second line should contain the result of float division, a//b.
+# No rounding or formatting is necessary.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+
+c = a // b
+d = a / b
+print(c)
+print(d)
