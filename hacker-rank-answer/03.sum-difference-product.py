86ad73b29568bfcd7085e33f03e8d15683bc996a
Hossein Eshraghi <eshraghi4office@gmail.com>
2023-01-08 18:40:21 +0330

    add answer 03 for hackerrank and dependencies

---
+++ hacker-rank-answer/03.sum-difference-product.py
@@ -0,0 +1,21 @@
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (first - second).
+# 3. The third line contains the product of the two numbers.
+
+# help:
+# The first line contains the first integer, a.
+# The second line contains the second integer, b.
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+
+c = a + b
+d = a - b
+e = a * b
+print(c)
+print(d)
+print(e)
